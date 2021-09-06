6c68a6f4265f552b474c5d013eff081147c6d0f3
luca <luca.bravo11@gmail.com>
2021-09-06 08:20:20 +0200

    Testuser in import.sql erstellt

--- m223-punchclock/src/main/resources/import.sql
+++ m223-punchclock/src/main/resources/import.sql
@@ -9,3 +9,5 @@ insert into TASK(ID, TASK_NAME) values ( 1, 'Testing' );
 insert into TASK(ID, TASK_NAME) values ( 2, 'Development Backend' );
 insert into TASK(ID, TASK_NAME) values ( 3, 'Development Frontend' );
 insert into TASK(ID, TASK_NAME) values ( 4, 'Support' );
+
+insert into APPLICATION_USER(ID, USERNAME, PASSWORD, DEPARTMENT_ID) values ( 1, 'luca', '1234567', 3 );
