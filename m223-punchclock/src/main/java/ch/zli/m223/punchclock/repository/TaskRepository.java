5045d9e298642f803a458e0f42dd736b6b7f864a
RajidPajakumaran <109346424+RajidPajakumaran@users.noreply.github.com>
2022-07-15 11:19:55 +0200

    Delete m223-punchclock directory

--- m223-punchclock/src/main/java/ch/zli/m223/punchclock/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.zli.m223.punchclock.repository;
-
-import ch.zli.m223.punchclock.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
